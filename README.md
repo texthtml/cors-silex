ccb18934f60f1c29e2071df7f41f54a5e45d746b
Mathieu Rochette <mathieu.rochette@prelinker.com>
2015-06-10 11:40:14 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# CORS-Silex
+
+ Cross-site HTTP requests middlware for [Silex](http://silex.sensiolabs.org/)
+
+[![Latest Stable Version](https://poser.pugx.org/texthtml/cors-silex/v/stable.svg)](https://packagist.org/packages/texthtml/cors-silex)
+[![License](https://poser.pugx.org/texthtml/cors-silex/license.svg)](https://packagist.org/packages/texthtml/cors-silex)
+[![Total Downloads](https://poser.pugx.org/texthtml/cors-silex/downloads.svg)](https://packagist.org/packages/texthtml/cors-silex)
+[![Scrutinizer Code Quality](https://scrutinizer-ci.com/g/texthtml/cors-silex/badges/quality-score.png?b=master)](https://scrutinizer-ci.com/g/texthtml/cors-silex/?branch=master)
+
+## Module installation
+
+In your project root folder
+
+1. `composer require texthtml/cors-silex`
+2. In your Silex container configuration, register the CORSMiddleware:
+
+```php
+use TH\Silex\CORS\CORSMiddleware;
+$app->after(new CORSMiddleware(
+    '*',
+    ['GET', 'DELETE', 'POST', 'PUT'],
+    ['Authorization', 'Content-Type']
+));
+```
---
+++ src/CORSMiddleware.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace TH\Silex\CORS;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+
+class CORSMiddleware
+{
+    private $origin;
+    private $methods;
+    private $headers;
+
+    /**
+     * @param string   $origin  orgin domain
+     * @param string[] $methods methods
+     * @param string[] $headers headers
+     */
+    public function __construct($origin, Array $methods, Array $headers)
+    {
+        $this->origin  = $origin;
+        $this->methods = implode(', ', $methods);
+        $this->headers = implode(', ', $headers);
+    }
+
+    public function __invoke(Request $request, Response $response)
+    {
+        if ($request->getMethod() === Request::METHOD_OPTIONS && $response->getStatusCode() === 405) {
+            $response = new Response('', Response::HTTP_NO_CONTENT);
+        }
+        $response->headers->set('Access-Control-Allow-Origin', $this->origin);
+        $response->headers->set('Access-Control-Allow-Methods', $this->methods);
+        $response->headers->set('Access-Control-Allow-Headers', $this->headers);
+        return $response;
+    }
+}
